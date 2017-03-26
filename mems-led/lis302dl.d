08e643ffed6a33cc1247157f63a23039384c471c
Chinmoy M <chinmoy.mhptra@gmail.com>
2017-03-26 11:58:38 +0530

    change led blinking using mems sensor

---
+++ mems-led/lis302dl.d
@@ -0,0 +1,4 @@
+.\lis302dl.o: lis302dl.c
+.\lis302dl.o: lis302dl.h
+.\lis302dl.o: accelero.h
+.\lis302dl.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ mems-led/lis3dsh.d
@@ -0,0 +1,4 @@
+.\lis3dsh.o: lis3dsh.c
+.\lis3dsh.o: lis3dsh.h
+.\lis3dsh.o: accelero.h
+.\lis3dsh.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
